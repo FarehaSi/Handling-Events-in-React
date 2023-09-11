0474508ee12bc0d405b8df6274e9aa8418e07dd1
FarehaSi <>
2023-09-11 21:48:08 +0000

    Conditional rendering

--- src/components/EventsClass.js
+++ src/components/EventsClass.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export class EventsClass extends Component {
-    clickHandler(){
-        console.log("Clicked the class button")
-    }
-    render() {
-        return (
-            <div>
-                <button onClick={this.clickHandler}>Click Me - Class Component</button>
-            </div>
-        )
-    }
-}
+// export class EventsClass extends Component {
+//     clickHandler(){
+//         console.log("Clicked the class button")
+//     }
+//     render() {
+//         return (
+//             <div>
+//                 <button onClick={this.clickHandler}>Click Me - Class Component</button>
+//             </div>
+//         )
+//     }
+// }
 
-export default EventsClass 
+// export default EventsClass 
