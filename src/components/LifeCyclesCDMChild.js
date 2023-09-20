97feb9b6aeb2a31c7239e278d33afe3e088fcf57
FarehaSi <>
2023-09-20 20:46:32 +0000

    componentDidUpdate

--- src/components/LifeCyclesCDMChild.js
+++ src/components/LifeCyclesCDMChild.js
@@ -1,40 +1,40 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-export class LifeCyclesCDMChild extends Component {
+// export class LifeCyclesCDMChild extends Component {
 
-  constructor(props) {
-    console.log('Child Constructor Called')
-    super(props)
+//   constructor(props) {
+//     console.log('Child Constructor Called')
+//     super(props)
 
-    this.state = {
-      data: 'Child Loading...',
-    }
-  }
+//     this.state = {
+//       data: 'Child Loading...',
+//     }
+//   }
 
-  getData() {
-    console.log('Child getData() called')
-    setTimeout(() => {
-      console.log('Child Data fetched!')
-      this.setState({
-        data: 'Child Loaded!'
-      })
-    }, 3000)
-  }
+//   getData() {
+//     console.log('Child getData() called')
+//     setTimeout(() => {
+//       console.log('Child Data fetched!')
+//       this.setState({
+//         data: 'Child Loaded!'
+//       })
+//     }, 3000)
+//   }
 
-  componentDidMount() {
-    console.log('Child Component mounted')
-    this.getData();
-  }
+//   componentDidMount() {
+//     console.log('Child Component mounted')
+//     this.getData();
+//   }
 
 
-  render() {
-    console.log('Child Render method called')
-    return (
-      <div>
-        <h1>{this.state.data}</h1>
-      </div>
-    )
-  }
-}
+//   render() {
+//     console.log('Child Render method called')
+//     return (
+//       <div>
+//         <h1>{this.state.data}</h1>
+//       </div>
+//     )
+//   }
+// }
 
-export default LifeCyclesCDMChild
+// export default LifeCyclesCDMChild
